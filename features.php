58511e71594e5ea6fa68e41602fdc525b71136d2
Unknown <olemortensen8@yahoo.dk>
2018-08-06 14:12:09 +0200

    Fixed Issue  causing the print not to print automagicly

--- features.php
+++ features.php
@@ -26,8 +26,8 @@
     function printDiv(divName) {
      var printContents = document.getElementById(divName).innerHTML;
      var originalContents = document.body.innerHTML;
-
      document.body.innerHTML = printContents;
+     window.print();
     }</script><?php } ?>
                         
                     </div>
