8a6807793c1740eabb8d6410b127162cb0bbab8d
Ole Vølund Skov Mortensen <olevsmortensen@dbc5radio.dk>
2022-08-24 15:15:47 +0200

    Fixed URL link

--- classes/autoload.php
+++ classes/autoload.php
@@ -2,6 +2,3 @@
 spl_autoload_register(function ($class_name) {
     include __DIR__ .'/'. $class_name . '.php';
 });
-$table = new Table();
-$db = new DB();
-?>
--- features.php
+++ features.php
@@ -1,17 +1,21 @@
-<?php $sideTitlen = 'KilometerJournalen i PHP'; 
- include('bootstrap.php');?>
+<?php $sideTitlen = 'KilometerJournalen i PHP';
+$db = new DB();
+include('bootstrap.php');
+$table = new Table(); ?>
     <main class="page">
         <section class="clean-block features">
             <div class="container">
                 <div class="block-heading">
-                    <h2 class="text-info" style="letter-spacing:0px;"><a href="https://bootstrapstudio.io/app/features.html"><strong>KILOMETER LISTE</strong></a><br></h2>
+                    <h2 class="text-info" style="letter-spacing:0;"><a href="features.php"><strong>KILOMETER LISTE</strong></a><br></h2>
                     <p>Her er et overblik over alle instastede kørseler</p>
                 </div>
                 <div>
                     <div class="table-responsive">
                         <div id="printableArea">
                                 <table class="table">
-                                    <?php $table->createTable($db->getDataToSql());
+                                    <?php
+                                    $table->createTable($db->getDataToSql());
+
                                     if($db->getDataToSql()){
                                     echo $table->getTable();
                                 }else{
@@ -20,7 +24,7 @@
                                 </table>
                         </div>
                         <?php if($db->getDataToSql()){ ?>
-    <button class="btn btn-primary" type="button" onclick="printDiv('printableArea')" ><img src="printer.svg"/></button>
+    <button class="btn btn-primary" type="button" onclick="printDiv('printableArea')" ><img src="printer.svg" alt="Printer"/></button>
     <div>Icons made by <a href="https://www.flaticon.com/authors/simpleicon" title="Printer">Printer</a> from <a href="https://www.flaticon.com/"     title="Flaticon">www.flaticon.com</a> is licensed by <a href="http://creativecommons.org/licenses/by/3.0/"     title="Creative Commons BY 3.0" target="_blank">CC 3.0 BY</a></div>
     <script>
     function printDiv(divName) {
