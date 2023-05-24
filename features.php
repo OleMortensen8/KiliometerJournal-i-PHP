dc5fffe0407ea4ca139786ccc3462467bb800f95
Ole V. S. Mortensen <olevsmortensen@dbc5radio.dk>
2023-05-24 19:14:46 +0200

    fixes indentation

--- features.php
+++ features.php
@@ -1,11 +1,13 @@
 <?php
 $sideTitlen = 'KilometerJournalen i PHP'; 
- include('bootstrap.php');?>
+ require "bootstrap.php"; ?>
     <main class="page">
         <section class="clean-block features">
             <div class="container">
                 <div class="block-heading">
-                    <h2 class="text-info" style="letter-spacing:0px;"><a href="https://bootstrapstudio.io/app/features.html"><strong>KILOMETER LISTE</strong></a><br></h2>
+                    <h2 class="text-info" style="letter-spacing:0px;">
+                        <a href="https://bootstrapstudio.io/app/features.html"><strong>KILOMETER LISTE</strong></a>
+                        <br></h2>
                     <p>Her er et overblik over alle instastede kørseler</p>
                 </div>
                 <div>
@@ -20,7 +22,8 @@ $sideTitlen = 'KilometerJournalen i PHP';
                                 ?>
                                 </table>
                         </div>
-                        <?php if($db->getDataToSql()){ ?>
+                        <?php 
+                        if ($db->getDataToSql()) { ?>
     <button class="btn btn-primary" type="button" onclick="printDiv('printableArea')" ><img src="printer.svg"/></button>
     <div>Icons made by <a href="https://www.flaticon.com/authors/simpleicon" title="Printer">Printer</a> from <a href="https://www.flaticon.com/"     title="Flaticon">www.flaticon.com</a> is licensed by <a href="http://creativecommons.org/licenses/by/3.0/"     title="Creative Commons BY 3.0" target="_blank">CC 3.0 BY</a></div>
     <script>
@@ -36,4 +39,4 @@ $sideTitlen = 'KilometerJournalen i PHP';
             </div>
         </section>
     </main>
-  <?php include('views/footer.php'); ?>
+  <?php require('views/footer.php'); ?>
