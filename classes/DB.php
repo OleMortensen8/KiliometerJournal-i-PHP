3860b7e63db56747792196d368b93051d4a1e2ce
Ole V. S. Mortensen <olevsmortensen@dbc5radio.dk>
2023-06-05 00:15:25 +0200

    setup new database class

--- classes/DB.php
+++ classes/DB.php
@@ -1,42 +1,49 @@
-<?php class DB{
-    
-    private $hostname = $_ENV['HOST'], //localhost
-        $username = $_ENV['USERNAME'],  //root
-        $password = $_ENV['PASSWORD'], //NULL
-        $options = array(PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8',
-    );
-        
-       public function DBCONNECT(){
-        try{
-            return new PDO(sprintf('mysql:host=%s;dbname=%s', getenv('HOST'), getenv('DATABASE')),
-            getenv('USERNAME'),
-            getenv('PASSWORD'), $this->options,
+<?php 
+
+class DB {
+    private $hostname, $database, $username, $password;
+
+    public function __construct() {
+        $this->hostname = $_ENV['HOST'];
+        $this->database = $_ENV['DATABASE'];
+        $this->username = $_ENV['USERNAME'];
+        $this->password = $_ENV['PASSWORD'];
+    }
+
+    public function DBCONNECT() {
+        try {
+            return new PDO(sprintf('mysql:host=%s;dbname=%s', $this->hostname, $this->database),
+                $this->username,
+                $this->password
             );
-        }catch(Exception $e){
-            return $e->getmessage();
+        } catch (PDOException $e) {
+            die("Database connection failed: " . $e->getMessage());
+        }
+    }
+    
+
+    public function sendsDataToSql($ini, $kmStart, $kmStop, $samledetal) {
+        try {
+            $statement = $this->DBCONNECT()->prepare("INSERT INTO Kiliometer_liste (initialer, kmStart, kmSlut, samledeKmTal) VALUES (?, ?, ?, ?)");
+            $statement->bindParam(1, $ini, PDO::PARAM_STR);
+            $statement->bindParam(2, $kmStart, PDO::PARAM_INT);
+            $statement->bindParam(3, $kmStop, PDO::PARAM_INT);
+            $statement->bindParam(4, $samledetal, PDO::PARAM_INT);
+            $statement->execute();
+            header("Location: features.php");
+        } catch (PDOException $e) {
+            echo "Error: " . $e->getMessage();
         }
     }
 
-    public function sendsDataToSql($ini, $kmStart, $kmStop, $samledetal){
-       $statement = $this->DBCONNECT()->prepare("INSERT INTO kiliometerliste (initialer, kmStart, kmSlut, samledeKmTal) VALUES (?, ?, ?, ?)");
-       $statement->bindParam(1, $ini, PDO::PARAM_STR);
-       $statement->bindParam(2, $kmStart, PDO::PARAM_INT);
-       $statement->bindParam(3, $kmStop, PDO::PARAM_INT);
-       $statement->bindParam(4, $samledetal, PDO::PARAM_INT);
-       $statement->execute();
-       header("Location: features.php");
-    }
-
-    public function getDataToSql(){
-        $statement = $this->DBCONNECT()->query("SELECT * FROM kiliometerliste
-        ORDER BY EntryID DESC LIMIT 5");
+    public function getDataToSql() {
+        $statement = $this->DBCONNECT()->query("SELECT * FROM Kiliometer_liste ORDER BY EntryID DESC LIMIT 5");
         return $statement->fetchAll(PDO::FETCH_ASSOC);
     }
 
-    public function deleteEntry($id){
-        $statement = $this->DBCONNECT()->prepare("DELETE FROM kiliometerliste WHERE EntryID=:id");
+    public function deleteEntry($id) {
+        $statement = $this->DBCONNECT()->prepare("DELETE FROM Kiliometer_liste WHERE EntryID=:id");
         $statement->bindParam(':id', $id, PDO::PARAM_INT);
         $statement->execute();
-    } 
+    }
 }
-?>
